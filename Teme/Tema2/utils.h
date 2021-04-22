c2a50b0232eca5261f7c3d4148af76d08ec73f63
Ubuntu 18 <danuberobotics@gmail.com>
2021-04-22 01:38:12 -0700

    subscriber done

---
+++ Teme/Tema2/utils.h
@@ -0,0 +1,12 @@
+#define MAX_LEN 1600
+#define ID_LEN 10
+#define STDIN 0 
+
+#define DIE(condition, message) \
+	do { \
+		if ((condition)) { \
+			fprintf(stderr, "[%d]: %s\n", __LINE__, (message)); \
+			perror(""); \
+			exit(1); \
+		} \
+	} while (0)
